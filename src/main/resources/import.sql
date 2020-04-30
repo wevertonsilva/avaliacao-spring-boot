0f0957b5f16d68d8575ac4d7d5600293ba516ba2
Weverton Silva <weverton-09@hotmail.com>
2020-04-30 01:46:58 -0300

    informações sobre projeto e candidato

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
---EFETUE A CRIACAO DOS SCRIPTS QUE IRAO FAZER A CARGA DOS DADOS
+INSERT INTO ESTUDANTE(nome,email,telefone,matricula,curso) VALUES('Xawoy','xawoy@tms.com.br',7777,123456,'Engenharia da Computação');
+INSERT INTO ESTUDANTE(nome,email,telefone,matricula,curso) VALUES('Furae','furae@tms.com.br',7778,678912,'Ciência da Computação');
+INSERT INTO ESTUDANTE(nome,email,telefone,matricula,curso) VALUES('Fupuy','fupuy@tms.com.br',7779,321654,'Engenharia da Computação');
+INSERT INTO ESTUDANTE(nome,email,telefone,matricula,curso) VALUES('Kuer','kuer@tms.com.br',7780,654987,'Análise de Dados');
+INSERT INTO ESTUDANTE(nome,email,telefone,matricula,curso) VALUES('Blias','blias@tms.com.br',7781,666131,'Ciência da Computação');
